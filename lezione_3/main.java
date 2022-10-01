8ab145dca6ba65e2fffaf5c98f574972993c4080
doddonicotina <74264293+doddonicotina@users.noreply.github.com>
2022-10-01 10:37:46 +0200

    Create main.java

---
+++ lezione_3/main.java
@@ -0,0 +1,13 @@
+public class main {
+	public static void mein()
+    {
+    MyStack s1 = new MyStack();
+    s1.push("xxz19200293");
+    s1.push("zxxds359850568");
+    for (int i=0; i<s1.getCount();i++)
+    {
+        System.out.println(s1.getElement(i));
+    }
+
+    }
+}
